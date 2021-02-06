90d2721348363f69adec5bb754f3cae79448c1f2
betelgeuze <ludo.bruynseels@gmail.com>
2021-02-06 21:23:11 +0100

    stuur AT+GMM => OK + antwoord.

--- AT-modemTest/Form1.Designer.cs
+++ AT-modemTest/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(322, 68);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Stuur AT";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(12, 350);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(730, 20);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtAnswer;
     }
 }
 
--- AT-modemTest/Form1.cs
+++ AT-modemTest/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AT_modemTest
@@ -15,6 +10,48 @@ namespace AT_modemTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static SerialPort MySerialPort { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Create a new SerialPort object with default settings.
+            MySerialPort = new SerialPort
+            {
+                PortName = "COM10",
+                BaudRate = 19200,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                Handshake = Handshake.None,
+                ReadTimeout = 500,
+                WriteTimeout = 500
+            };
+
+            MySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            MySerialPort.Open();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            MySerialPort.Write("AT+GMM\r");
+        }
+
+        private static void DataReceivedHandler(
+            object sender,
+            SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.Write(indata);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MySerialPort.Close();
+            Thread.Sleep(500);
         }
     }
 }
